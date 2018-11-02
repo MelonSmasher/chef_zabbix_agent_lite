23298ae5e21a64661f2d8fedee7d5449a72f7e32
Alex Markessinis <markea125@gmail.com>
2018-11-01 20:56:13 -0400

    updated version

--- metadata.rb
+++ metadata.rb
@@ -8,7 +8,7 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 issues_url 'https://github.com/melonsmasher/chef_zabbix_agent_lite/issues' if respond_to?(:issues_url)
 source_url 'https://github.com/melonsmasher/chef_zabbix_agent_lite' if respond_to?(:source_url)
 
-version '0.1.0'
+version '0.1.1'
 
 chef_version ">= 12.11" if respond_to?(:chef_version)
 
@@ -16,7 +16,7 @@ supports 'ubuntu', '>= 14.04'
 supports 'debian', '>= 8.0'
 supports 'centos', '>= 7.0'
 supports 'redhat', '>= 7.0'
-supports 'oracle linux', '>= 7.0'
+supports 'oracle', '>= 7.0'
 supports 'windows'
 
 depends 'chocolatey', '~> 2.0.0'
